8e665d642f0c9e17285c170f1269c0b77be869d3
Ethical-Hacker-est <estcompany5@gmail.com>
2026-04-19 23:44:26 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+import hashlib
+import time
+
+URLS = [
+    "",
+    ""
+]
+
+CHECK_INTERVAL = 180  # 3 minut
+BOT_TOKEN = ""
+CHAT_ID = ""
+# ======================
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+}
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": message}
+    requests.post(url, data=data)
+
+
+def get_hash(url):
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        content = soup.get_text()
+        return hashlib.md5(content.encode()).hexdigest()
+    except:
+        return None
+
+
+print("Monitoring started...")
+
+
+last_hashes = {}
+
+for url in URLS:
+    last_hashes[url] = get_hash(url)
+
 while True:
-    kamal_data = int(input("Please write ur name:")
-                     if kamal_data.isalpha():
-                     break
-                     else:
-                     continue
-    print("Hello dear MR.",kamal_data)
-                     #banananananaa
+    time.sleep(CHECK_INTERVAL)
+
+    for url in URLS:
+        new_hash = get_hash(url)
+        if new_hash is None:
+            continue
+
+        if new_hash != last_hashes[url]:
+            print(f" CHANGE DETECTED!!!!!!!: {url}")
+            send_telegram(f" Website changed:\n{url}")
+            last_hashes[url] = new_hash
+        else:
+            print(f"No change: {url}")
