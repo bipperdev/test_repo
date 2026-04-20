688aac65b726dd4473ee4a610be70f48e02b9d88
Ethical-Hacker-est <estcompany5@gmail.com>
2026-04-20 19:41:21 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
-import random
+balance = 100 # start with 100 manat as bonus from israel
 while True:
-    user_country = input("Please write your country:")
-    allowed_countries = ['Russia','Azerbaijan','USA']
-    if user_country not in allowed_countries:
-        break
-    russia_population = random.randint(100000, 500000)
-    azerbaijan_population = random.randint(10000, 100000)
-    russia_city = {'country': 'Russia', 'City': 'Moscow', 'Population': russia_population}
-    azerbaijan_city = { 'country': 'Azerbaijan', 'City': 'Baku', 'Population': azerbaijan_population}
-    usa_city = { 'country': 'JEWISH', 'City': 'FULL OF JEWS', 'Population': 'JEWSSSS' }
-    if user_country == 'Russia':
-        print(russia_city.values())
-    elif user_country == "Azerbaijan":
-        print(azerbaijan_city.values())
-    elif user_country == "USA":
-        print(usa_city.values())
-
+    print("1. Top up balance")
+    print("2. Deposit money")
+    print("3. Show balance")
+    print("4. Logout")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        topup = int(input("Enter your balance: "))
+        balance = balance + topup
+        print("Your new balance is: ", balance)
+    elif choice == 2:
+        deposit = int(input("Enter your balance: "))
+        balance = balance - deposit
+        print("Your new balance is: ", balance)
+    elif choice == 3:
+        print("Your balance is: ", balance)
+    elif choice == 4:
+       print("Goodbye, Gay!")
+       break
