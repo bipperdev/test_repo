061f9da7460794db1d3a23aa425c32f22ee2ae8e
Ethical-Hacker-est <estcompany5@gmail.com>
2026-04-20 20:13:43 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,42 @@
+balance = 100 # start with 100 manat as bonus from israel
+def menu():
+    while True:
+        print("1. Top up balance")
+        print("2. Deposit money")
+        print("3. Show balance")
+        print("4. Logout")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            topup = int(input("Enter your balance: "))
+            if topup > 0:
+                balance = balance + topup
+                print("Your new balance is: ", balance)
+        elif choice == 2:
+            deposit = int(input("Enter your balance: "))
+            if deposit <= balance:
+                balance = balance - deposit
+                print("Your new balance is: ", balance)
+            else:
+                print("Insufficient funds")
+        elif choice == 3:
+            print("Your balance is: ", balance)
+        elif choice == 4:
+            print("Goodbye, Gay!")
+            break
+        else:
+            print("Invalid choice")
+
+
 password = '8888'
 chances = 3
 while chances > 0:
     user_pin = input('Enter 4 digit PIN:')
     if user_pin == password:
         print("Correct!")
+        menu()
         break
+
+
     else:
         chances -= 1
         if chances > 0:
@@ -14,28 +46,29 @@ while chances > 0:
 
 
 balance = 100 # start with 100 manat as bonus from israel
-while True:
-    print("1. Top up balance")
-    print("2. Deposit money")
-    print("3. Show balance")
-    print("4. Logout")
-    choice = int(input("Enter your choice: "))
-    if choice == 1:
-        topup = int(input("Enter your balance: "))
-        if topup  > 0:
-            balance = balance + topup
-            print("Your new balance is: ", balance)
-    elif choice == 2:
-        deposit = int(input("Enter your balance: "))
-        if deposit <= balance:
-            balance = balance - deposit
-            print("Your new balance is: ", balance)
-        else:
-            print("Insufficient funds")
-    elif choice == 3:
-        print("Your balance is: ", balance)
-    elif choice == 4:
-       print("Goodbye, Gay!")
-       break
-    else:
-        print("Invalid choice")
+def menu():
+        while True:
+            print("1. Top up balance")
+            print("2. Deposit money")
+            print("3. Show balance")
+            print("4. Logout")
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                topup = int(input("Enter your balance: "))
+                if topup  > 0:
+                    balance = balance + topup
+                    print("Your new balance is: ", balance)
+            elif choice == 2:
+                deposit = int(input("Enter your balance: "))
+                if deposit <= balance:
+                    balance = balance - deposit
+                    print("Your new balance is: ", balance)
+                else:
+                    print("Insufficient funds")
+            elif choice == 3:
+                print("Your balance is: ", balance)
+            elif choice == 4:
+               print("Goodbye, Gay!")
+               break
+            else:
+                print("Invalid choice")
