d9a59404557e7e42153276955edfb846a7eaad68
BipperDev <bipperbananas3567@gmail.com>
2026-04-20 20:15:50 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,52 +1,10 @@
-balance = 100 # start with 100 manat as bonus from israel
-def menu():
-    while True:
-        print("1. Top up balance")
-        print("2. Deposit money")
-        print("3. Show balance")
-        print("4. Logout")
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
-            topup = int(input("Enter your balance: "))
-            if topup > 0:
-                balance = balance + topup
-                print("Your new balance is: ", balance)
-        elif choice == 2:
-            deposit = int(input("Enter your balance: "))
-            if deposit <= balance:
-                balance = balance - deposit
-                print("Your new balance is: ", balance)
-            else:
-                print("Insufficient funds")
-        elif choice == 3:
-            print("Your balance is: ", balance)
-        elif choice == 4:
-            print("Goodbye, Gay!")
-            break
-        else:
-            print("Invalid choice")
-
-
 password = '8888'
 chances = 3
 while chances > 0:
     user_pin = input('Enter 4 digit PIN:')
     if user_pin == password:
-        print("Correct!")
-        menu()
-        break
-
-
-    else:
-        chances -= 1
-        if chances > 0:
-            print("Wrong pin! you have:",chances,"Attempts left")
-        else:
-            print("Sorry you have:",chances,"Attempts left")
-
-
-balance = 100 # start with 100 manat as bonus from israel
-def menu():
+        print("Correct!Acsess allowed")
+        balance = 100  # start with 100 manat as bonus from israel
         while True:
             print("1. Top up balance")
             print("2. Deposit money")
@@ -55,7 +13,7 @@ def menu():
             choice = int(input("Enter your choice: "))
             if choice == 1:
                 topup = int(input("Enter your balance: "))
-                if topup  > 0:
+                if topup > 0:
                     balance = balance + topup
                     print("Your new balance is: ", balance)
             elif choice == 2:
@@ -68,7 +26,18 @@ def menu():
             elif choice == 3:
                 print("Your balance is: ", balance)
             elif choice == 4:
-               print("Goodbye, Gay!")
-               break
+                print("Goodbye, Gay!")
+                break
             else:
                 print("Invalid choice")
+        break
+    else:
+        chances -= 1
+        if chances > 0:
+            print("Wrong pin! you have:",chances,"Attempts left")
+        else:
+            print("Sorry you have:",chances,"Attempts left")
+            break
+        
+
+
